180667480062caa8b0e37f219413bb48c01d55b6
VincentJiang <jxs471494539@gmail.com>
2014-03-07 17:14:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## README
+
+### build with
+
+* ruby 2.1.1
+* rails 4.0.3
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecoit::Application.config.secret_key_base = 'edeecde8b6b9dd4c89ff85ada87b8ca02295124f61635bb31dd1dbd3169acce79fc83b49dd78fee2362454d646722da730e6afaf1c50409bc24eb22dcc91e17a'
